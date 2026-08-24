ad9539657e3b06d242e41d248b58e61370e6f8c0
Lei <gabriel_sl19798@hotmail.com>
2026-08-24 20:31:30 +1200

    function based view.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 
-from blog.views import home, categories
+from blog.views import home, categories, posts, category_detail, post_detail, category_create, category_update, \
+    category_delete, post_create
 
 urlpatterns = [
     path("", home, name="home"),
     path("categories/", categories, name="categories"),
+    path("posts/", posts, name="posts"),
+    path("category/<int:category_id>/", category_detail, name="category_detail"),
+    path("post/<int:post_id>/", post_detail, name="post_detail"),
+    path("category/create/", category_create, name="category_create"),
+    path("category/update/<int:category_id>/", category_update,
+         name="category_update"),
+    path("category/delete/", category_delete,
+         name="category_delete"),
+    path("post/create/", post_create, name="post_create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-from blog.models import Category
+from blog.models import Category, Post
 
 
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
 
 def categories(request):
     categories = Category.objects.all()
@@ -13,3 +15,66 @@ def categories(request):
                   'blog/categories.html',
                   {'categories': categories})
 
+
+def posts(request):
+    posts = Post.objects.all()
+    users = User.objects.all()
+    categories = Category.objects.all()
+    return render(request,
+                  'blog/posts.html',
+                  {
+                      'posts': posts,
+                      'users': users,
+                      'categories': categories
+                  })
+
+
+def category_detail(request, category_id):
+    category = Category.objects.get(id=category_id)
+    return render(request, "blog/category_detail.html",
+                  {'category': category})
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, "blog/post_detail.html",
+                  {'post': post})
+
+
+def category_create(request):
+    category_name = request.POST['name']
+    category = Category.objects.create(name=category_name)
+    return redirect('categories')
+
+
+def category_update(request, category_id):
+    category = Category.objects.get(id=category_id)
+    category.name = request.POST['name']
+    category.save()
+    return redirect('categories')
+
+
+def category_delete(request):
+    category_id = request.POST['category_id']
+    category = Category.objects.get(id=category_id)
+    category.delete()
+    return redirect('categories')
+
+
+def post_create(request):
+    title = request.POST['title']
+    header_image = request.FILES['header_image']
+    title_tag = request.POST['title_tag']
+    author = User.objects.get(id=request.POST['author'])
+    body = request.POST['body']
+    snippet = request.POST['snippet']
+    category = Category.objects.get(id=request.POST['category'])
+
+    post = Post.objects.create(title=title,
+                               header_image=header_image,
+                               title_tag=title_tag,
+                               author=author,
+                               body=body,
+                               snippet=snippet,
+                               category=category)
+    return redirect('posts')
